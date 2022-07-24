e1697a62e8116ba1821e211a14a077a6c42517ea
Alhaimouni <m.alhaumouni@gmail.com>
2022-07-24 22:08:45 +0300

    Adding work

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <>
+            <p> All rigths reserved @Mohammad Al-haimouni</p>
+            </>
+        )
+    }
+}
+
+export default Footer;
