8955b51a3a6257fae8bff190c1973968cb1f29e6
Alhaimouni <m.alhaumouni@gmail.com>
2022-07-26 17:34:26 +0300

    aadding BootStrap

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Main from './Main';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,29 +1,40 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import "./HornedBeast.css";
+
+
 class HornedBeast extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {counter : 0}
+        this.state = { counter: 0 }
     }
 
     addCount = () => {
 
-        this.setState({counter : this.state.counter+1})
+        this.setState({ counter: this.state.counter + 1 })
 
     }
 
 
-
-
     render() {
-        
-        return(
-            <>
-            <h2>{this.props.title}</h2>
-            <p>Likes = {this.state.counter}</p>
-            <img onClick={this.addCount} src={this.props.imgUrl} alt={this.props.description} height="200" />
-            <p>{this.props.description}</p>
-            </>
+
+        return (
+
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={this.props.imgUrl} />
+                <Card.Body>
+                    <Card.Title>{this.props.title}</Card.Title>
+                    <Card.Text>
+                    {this.props.description}
+                    </Card.Text>
+                    <Button className="b" onClick={this.addCount} variant="primary"> ❤️ {this.state.counter}</Button>
+                </Card.Body>
+            </Card>
+
+
+
         )
     }
 }
